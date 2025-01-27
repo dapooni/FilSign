77147b58704f55ba0b7215b3da837769fde236f6
LetClar <jcbertulfo25@gmail.com>
2025-01-27 19:11:43 +0800

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image } from 'react-native';
+import { CameraType, CameraView, useCameraPermissions } from 'expo-camera';
+import { useRef, useState } from 'react';
+
+import SettingDropdown from './navigation/SettingDropDown';
+import TranslateDropdown from './navigation/TranslateDropDown'
 
 export default function App() {
+  const [facing, setFacing] = useState<CameraType>('front');
+  const [permission, requestPermission] = useCameraPermissions();
+  const cameraRef = useRef<CameraView | null>(null);
+  const [isPressed, setIsPressed] = useState(false);
+
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'front' ? 'back' : 'front'));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CameraView style={styles.camera} facing={facing} ref={cameraRef}>
+        <View style={styles.buttonContainer}>
+          {/* Setting Button */}
+          <SettingDropdown />
+          {/* Camera Rotate */}
+          <TouchableOpacity onPress={toggleCameraFacing}>
+            <Image style={styles.image} source={require('./assets/images/light-camrotate.png')} />
+          </TouchableOpacity>
+        </View>
+      </CameraView>
+      {/* Two-Way Communication Choices  */}
+      {/* FROM */}
+      <View style={styles.anotherContainer}>
+        <View style={styles.translateContainer}>
+          <TranslateDropdown
+            data={[
+              { value: "FSL", label: "FSL GESTURE" }, 
+              { value: "TS", label: "TEXT/SPEECH" },
+            ]}
+            onChange={console.log}
+            placeholder="FROM"
+          />
+        </View>
+        {/* TO */}
+        <View style={styles.translateContainer}>
+          <TranslateDropdown
+            data={[
+              { value: "FSL", label: "FSL GESTURE" }, 
+              { value: "TS", label: "TEXT/SPEECH" },
+            ]}
+            onChange={console.log}
+            placeholder="TO"
+          />
+        </View>
+      </View>
+      {/* Text Input/Output Container */}
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>
+          HELLO MY NAME IS PHIL FROM LEG. CITY, ALBAY, PHILIPPINES
+        </Text>
+        {/* Speaker/Microphone*/}
+        <TouchableOpacity style={styles.speakerButton} onPress={() => setIsPressed((prevState) => !prevState)} >
+        <Image style={styles.image} source={ isPressed
+              ? require('./assets/images/speaker-pressed.png') 
+              : require('./assets/images/speaker.png')}/>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +84,71 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: '#0C4CBE'
   },
+  camera: {
+    flex: 0,
+    height: '80%',
+    width: '100%', 
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 20,
+    paddingTop: 44,
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+  },
+  image: {
+    width: 50,
+    height: 50,
+  },
+  textContainer: {  // Box container for Text Input/Output
+    width: '90%', 
+    height: 150,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+    backgroundColor: '#96B4E8',
+    borderRadius: 30,
+    padding: 15,
+    shadowColor: '#000', //Shadow
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.2,
+    shadowRadius: 6.27,
+    elevation: 10, 
+    alignSelf: 'center',
+    marginTop: -15
+  },
+  text: {   // Text container for Text Input/Output
+    flex: 1,
+    fontSize: 16,
+    color: '#000',
+    padding: 10,
+    fontFamily: 'AlbertSans-Medium'
+  },
+  speakerButton: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10
+  },
+  anotherContainer: { // Box container for translate
+    width: '90%',
+    backgroundColor: 'rgba(8, 48, 129, 0.7)',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    borderRadius: 30,
+    paddingBottom: 15,
+    alignSelf: 'center',
+    marginTop: -100,
+    marginBottom: 30,
+    height: 55,
+  },
+  translateContainer: {  // Box container for per choice of translate
+    width: '50%', 
+    height: 55,
+    borderRadius: 30,
+  }
 });
---
+++ navigation/SettingDropDown.tsx
@@ -0,0 +1,62 @@
+import React, { useState, useRef } from 'react';
+import { Animated, TouchableOpacity, Image, StyleSheet, View } from 'react-native';
+
+export default function Dropdown() {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownOpacity = useRef(new Animated.Value(0)).current;
+
+  function toggleDropdown() {
+    if (showDropdown) {
+      // Fade out animation
+      Animated.timing(dropdownOpacity, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start(() => setShowDropdown(false));
+    } else {
+      setShowDropdown(true);
+      // Fade in animation
+      Animated.timing(dropdownOpacity, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    }
+  }
+
+  return (
+    <View>
+      {/* Setting Button */}
+      <TouchableOpacity onPress={toggleDropdown}>
+        <Image style={styles.image} source={require('../assets/images/light-setting.png')} />
+      </TouchableOpacity>
+      {showDropdown && (
+        <Animated.View style={[styles.dropdownContainer, { opacity: dropdownOpacity }]}>
+          {/* Dark/Light Mode Button */}
+          <TouchableOpacity style={styles.dropdownButton}>
+            <Image style={styles.image} source={require('../assets/images/dark-mode.png')} />
+          </TouchableOpacity>
+          {/* Tutorial Button */}
+          <TouchableOpacity style={styles.dropdownButton}>
+            <Image style={styles.image} source={require('../assets/images/light-tutorial.png')} />
+          </TouchableOpacity>
+        </Animated.View>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  dropdownContainer: {
+    position: 'absolute',
+    top: 50,
+  },
+  dropdownButton: {
+    padding: 2,
+    justifyContent: 'flex-start',
+  },
+  image: {
+    width: 50,
+    height: 50,
+  },
+});
